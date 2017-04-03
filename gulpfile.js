2dcc60dba90760046c5aea0da7eca67d418bb2a4
Rafael Grillo <rafael@commodityvectors.com>
2017-04-03 12:43:10 +0100

    Add plumber to avoid breaking gulp streams

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
+const plumber = require('gulp-plumber');
 
 gulp.task('build', () => {
   return gulp.src(['./src/**/*.js'])
+    .pipe(plumber())
     .pipe(babel({ presets: ['es2015'] }))
     .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('lint', () => {
-  return gulp.src(['./src/**/*.js']).pipe(eslint()).pipe(eslint.format());
+  return gulp.src(['./src/**/*.js'])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
 gulp.task('watch', () => {
